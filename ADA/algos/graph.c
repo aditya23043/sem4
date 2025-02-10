d2fb264818ef3104854f4312b7b6704e7de9e2c6
Aditya Gautam <adityascottish27@gmail.com>
2025-02-10 11:05:43 +0530

    psd class

---
+++ ADA/algos/graph.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * Graph is denoted by G(V,E)
+ * V : vertices
+ * E : edges
+ */
+
+/* Undirected graphs are represented as an Adjacency Matrix */
+
+int main(int argc, char **argv) { return 0; }
---
+++ ADA/algos/pancake_sorting.c
@@ -0,0 +1,4 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *pancakeSort(int *arr, int arrSize, int *returnSize) {}
